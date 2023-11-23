024f4bf7f38f6d999792d4fab3749dc941e296ae
Yevhenii <jenmikitenko@gmail.com>
2023-11-23 17:50:37 +0200

    fix segment

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,5 +1,6 @@
 import { supabase } from "@/configs/postsConfigs"
 import { NextResponse } from 'next/server'
+export const dynamic = 'force-dynamic'
 
 export async function GET(request: Request) {
     try {
