44ea39bd37a8e349546d2a197f246b67e427e940
Yevhenii <jenmikitenko@gmail.com>
2023-11-23 17:52:00 +0200

    fix segment2.0

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,6 +1,5 @@
 import { supabase } from "@/configs/postsConfigs"
 import { NextResponse } from 'next/server'
-export const dynamic = 'force-dynamic'
 
 export async function GET(request: Request) {
     try {
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import HomePosts from "@/components/Home";
 import { Text } from "@mantine/core";
 
-
-
+export const dynamic = 'force-dynamic'
 
 export default async function HomePage() {
    const posts = await fetch("http://localhost:3000/api/posts",{ cache: 'no-store' })
