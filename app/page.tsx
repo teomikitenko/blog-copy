51aa2e00a3e115e4e0d3d5c7c6130d58fe75d8d5
Yevhenii <jenmikitenko@gmail.com>
2023-12-02 00:19:45 +0200

    some feauters

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,13 @@ import { Text } from "@mantine/core";
 import { takeCommunityPosts } from "@/configs/postsConfigs";
 
 export default async function HomePage() {
-        const posts = await fetch("https://blog-copy-kitt.vercel.app/api/posts", {
+/*         const posts = await fetch("https://blog-copy-kitt.vercel.app/api/posts", {
     cache: "no-store",
-  });      
+  });   */    
+  const posts = await fetch("http://localhost:3000/api/posts", {
+  cache: "no-store",  
+}
+);
  
   const data = await posts.json(); 
   return (
